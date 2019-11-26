2826279796e50658fd3c86a3b5f82f7ff129e9b8
Edward J Kim <kimedw@amazon.com>
2019-11-26 12:11:52 -0800

    Use Python 3.6 installed with conda (#71)

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     install_requires=read("requirements.txt"),
