46e8896370f35b4334c1acd20e929232fc63f8db
Haifeng Li <lihaife@amazon.com>
2021-03-08 13:00:05 -0500

    check rank==0 in SaveIntermediateModelCallBack

--- src/sagemaker_xgboost_container/checkpointing.py
+++ src/sagemaker_xgboost_container/checkpointing.py
@@ -398,5 +398,18 @@ class SaveIntermediateModelCallBack(xgb.callback.TrainingCallback):
         self._callback = SaveIntermediateModel(intermediate_model_dir, model_name)
 
     def after_iteration(self, model, epoch, evals_log) -> bool:
-        self._callback._save_intermediate_model(model)
+        if rabit.get_rank() == 0:
+            self._callback._save_intermediate_model(model)
         return False
+
+
+class SaveCrossValidationModelsCallBack(xgb.callback.TrainingCallback):
+    """The new implementation of callback functions from 1.3."""
+    def __init__(self, model_list):
+        self._model_list = model_list
+
+    def after_training(self, model):
+        if rabit.get_rank() == 0:
+            print(model)
+            self._model_list.append(model)
+        return model
