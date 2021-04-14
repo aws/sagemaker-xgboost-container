7cc7433ae09e0b3f615b3d9caced2a15fdf4d3fb
Huibin Shen <huibishe@amazon.com>
2021-04-14 22:38:40 +0200

    remove unused import in custom_metrics.py

--- src/sagemaker_xgboost_container/metrics/custom_metrics.py
+++ src/sagemaker_xgboost_container/metrics/custom_metrics.py
@@ -10,8 +10,6 @@
 # distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-from collections import Counter
-
 import numpy as np
 from sklearn.metrics import accuracy_score, f1_score, mean_squared_error, r2_score
 
