4a99a8a4485b66edf631db8b308c93bf7cb923ef
Haixin Wang <98612668+haixiw@users.noreply.github.com>
2022-08-26 09:34:47 -0700

    Mix-in smdebug.xgboost.Hook and xgb.callback.TrainingCallback for callback compatibility in XGB-3.0.0+ (#301)
    * remove smdebugger hook callback for xgboost>=1.3.0
    
    * resolve comments
    
    * wrap sagemaker debugger Hook with new callback style
    
    * code formatting
    
    * still use hook_from_config to initialize Hook

--- src/sagemaker_xgboost_container/callback.py
+++ src/sagemaker_xgboost_container/callback.py
@@ -1,13 +1,36 @@
 import logging
 from smdebug.xgboost import Hook
+import xgboost as xgb
 
 
 logger = logging.getLogger(__name__)
 
 
+class SmeDebugHook(xgb.callback.TrainingCallback, Hook):
+    """Mix-in callback class. smedebug.xgboost.Hook uses legacy callback style
+    and since XGB-3.0.0 mixing legacy callback instances with new TrainingCallback
+    instances is not allowed.
+
+    See: https://github.com/dmlc/xgboost/blob/v1.3.0/python-package/xgboost/training.py#L92-L93
+
+    :param hyperparameters: Dict of hyperparamters.
+                            Same as `params` in xgb.train(params, dtrain).
+    :param train_dmatrix: Training data set.
+    :param val_dmatrix: Validation data set.
+    """
+    def __init__(self, json_config_path, hyperparameters,
+                 train_dmatrix, val_dmatrix):
+        self = self.hook_from_config(json_config_path)
+        self.hyperparameters = hyperparameters
+        self.train_data = train_dmatrix
+        if val_dmatrix is not None:
+            self.validation_data = val_dmatrix
+
+
 def add_debugging(callbacks, hyperparameters, train_dmatrix,
                   val_dmatrix=None, json_config_path=None):
     """Add a sagemaker debug hook to a list of callbacks.
+
     :param callbacks: List of callback functions.
     :param hyperparameters: Dict of hyperparamters.
                             Same as `params` in xgb.train(params, dtrain).
@@ -17,13 +40,9 @@ def add_debugging(callbacks, hyperparameters, train_dmatrix,
                              instead of default config file.
     """
     try:
-        hook = Hook.hook_from_config(json_config_path)
-        hook.hyperparameters = hyperparameters
-        hook.train_data = train_dmatrix
-        if val_dmatrix is not None:
-            hook.validation_data = val_dmatrix
-        callbacks.append(hook)
+        hook = SmeDebugHook(json_config_path, hyperparameters, train_dmatrix, val_dmatrix)
         logging.info("Debug hook created from config")
     except Exception as e:
         logging.debug("Failed to create debug hook", e)
-        return
+    else:
+        callbacks.append(hook)
