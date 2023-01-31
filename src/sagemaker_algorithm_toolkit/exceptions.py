96865403b7e9f3ef1e9f61f974fd92a9ba5c6057
haixiw <haixiw@amazon.com>
2023-01-31 18:19:58 +0000

    test update

--- src/sagemaker_algorithm_toolkit/exceptions.py
+++ src/sagemaker_algorithm_toolkit/exceptions.py
@@ -90,4 +90,4 @@ class PlatformError(BaseToolkitError):
 
     def __init__(self, message=None, caused_by=None):
         super(PlatformError, self).__init__(message, caused_by)
-        print("test1")
+        print("test2")
