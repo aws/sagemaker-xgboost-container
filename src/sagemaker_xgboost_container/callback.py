94e4eac80dd9237111cbd4c3b6701dd2b21c6c61
Malav Shastri <malavhs@amazon.com>
2023-02-16 23:12:52 -0500

    Bump smdebug version to 1.0.28

--- src/sagemaker_xgboost_container/callback.py
+++ src/sagemaker_xgboost_container/callback.py
@@ -11,27 +11,6 @@ from smdebug.xgboost import Hook
 logger = logging.getLogger(__name__)
 
 
-class SmeDebugHook(xgb.callback.TrainingCallback, Hook):
-    """Mix-in callback class. smedebug.xgboost.Hook uses legacy callback style
-    and since XGB-3.0.0 mixing legacy callback instances with new TrainingCallback
-    instances is not allowed.
-
-    See: https://github.com/dmlc/xgboost/blob/v1.3.0/python-package/xgboost/training.py#L92-L93
-
-    :param hyperparameters: Dict of hyperparamters.
-                            Same as `params` in xgb.train(params, dtrain).
-    :param train_dmatrix: Training data set.
-    :param val_dmatrix: Validation data set.
-    """
-
-    def __init__(self, json_config_path, hyperparameters, train_dmatrix, val_dmatrix):
-        self = self.hook_from_config(json_config_path)
-        self.hyperparameters = hyperparameters
-        self.train_data = train_dmatrix
-        if val_dmatrix is not None:
-            self.validation_data = val_dmatrix
-
-
 def add_debugging(callbacks, hyperparameters, train_dmatrix, val_dmatrix=None, json_config_path=None):
     """Add a sagemaker debug hook to a list of callbacks.
 
@@ -44,12 +23,16 @@ def add_debugging(callbacks, hyperparameters, train_dmatrix, val_dmatrix=None, j
                              instead of default config file.
     """
     try:
-        hook = SmeDebugHook(json_config_path, hyperparameters, train_dmatrix, val_dmatrix)
+        hook = Hook.hook_from_config(json_config_path)
+        hook.hyperparameters = hyperparameters
+        hook.train_data = train_dmatrix
+        if val_dmatrix is not None:
+            hook.validation_data = val_dmatrix
+        callbacks.append(hook)
         logging.info("Debug hook created from config")
     except Exception as e:
         logging.debug("Failed to create debug hook", e)
-    else:
-        callbacks.append(hook)
+        return
 
 
 def add_sigterm_handler(model_dir, is_master):
