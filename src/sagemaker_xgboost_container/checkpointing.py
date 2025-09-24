0e2a074ab47dbbbcdcbb37a31be245a5f71ffeb3
Li Ning <lninga@amazon.com>
2025-09-23 19:08:54 -0700

    test rabit.tracker_print migration

--- src/sagemaker_xgboost_container/checkpointing.py
+++ src/sagemaker_xgboost_container/checkpointing.py
@@ -7,7 +7,7 @@ import threading
 
 import xgboost as xgb
 from typing import Optional
-from xgboost.dask import DaskDMatrix, train
+from xgboost import collective
 from xgboost.callback import EvaluationMonitor
 from xgboost.core import XGBoostError
 
@@ -116,7 +116,8 @@ class PrintCheckpoint(xgb.callback.TrainingCallback):
                         score = log[-1]
                     msg += evaluation_monitor._fmt_metric(data, metric_name, score, stdv)
             msg += "\n"
-            rabit.tracker_print("[%d]\t%s\n" % (i + self.start_iteration, msg))
+            with collective.CommunicatorContext():
+                track_print("[%d]\t%s\n" % (i + self.start_iteration, msg))
 
 
 def print_checkpointed_evaluation(end_iteration, iteration=0, rank=0, period=1, show_stdv=True, start_iteration=0):
